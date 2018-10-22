dc79395b1ad162ceaee75085d6dfd625b808da54
EvertonLWF <38596722+EvertonLWF@users.noreply.github.com>
2018-10-22 18:14:03 -0300

    Add files via upload

--- moodle_atividade/script.js
+++ moodle_atividade/script.js
@@ -1,8 +1,29 @@
+x = 1;
 $(function(){
     $('#descricao').bind('mouseover',function(){
         $('#descricao p ').html('Qual é a saudação a ser usada em cada uma das situações a seguir? Selecione a saudação de acordo com o momento do dia e o que está acontecendo.');
     });
     $('#descricao').bind('mouseout',function(){
         $('#descricao p ').html('<p>What\'s the greeting to be used en each of the following situations? Select it according to the moment of the day and what is happening. <a href="#">Português</a></p>');
+    });
+    $('.btn-1').bind('click',function(){
+    	$('.form-'+x).hide();
+    	x++;
+
+    	$('.form-'+x).show();
+    	$('.text-center').html(x+' out of 5');
+    	if(x==6){
+    		$('.col-md-4').hide();
+    		$('.text-center').hide();
+    		$('.right').attr('disabled','');
+    		$('.left').attr('disabled','');
+       	}
+    	$('.btn-3').bind('click',function(){
+    		$('.form-'+x).hide();
+    		x=1;
+    		$('.text-center').show();
+    		$('.text-center').html(x+' out of 5');
+    		$('.form-'+x).show();
+    	});
     });
 });
