a70ac7a620f006b2a2cebe1946885e3a518173a6
Lucas Büttow <35873132+lthbuttow@users.noreply.github.com>
2018-12-14 18:39:08 -0200

    Add files via upload

---
+++ 9_countries_nationalities/script.js
@@ -0,0 +1,53 @@
+x = 1;
+$(function () {
+	$('#descricao').bind('mouseover', function () {
+		$('#descricao p ').html('Momento geografia! Olhe as bandeiras com diferentes países ao redor do mundo e escreva o nome da nacionalidade correspondente. Boa viagem!');
+	});
+	$('#descricao').bind('mouseout', function () {
+		$('#descricao p ').html('Geography time! Look at the flags from different countries around the world and write the name of the corresponding nationality. Have a good trip!<a href="#">Português</a>');
+	});
+	$('.btn-1').bind('click', function () {
+		$('.form-' + x).hide();
+		x++;
+
+		$('.form-' + x).show();
+		$('.text-center').html(x + ' <span lang="en">out of 4</span>');
+		if (x == 6) {
+			$('#descricao').hide();
+			$('.col-md-4').hide();
+		}
+		// $('.btn-3').bind('click', function () {
+		// 	$('.form-' + x).hide();
+		// 	x = 1;
+		// 	$('.text-center').show();
+		// 	$('.text-center').html(x + ' <span lang="en">out of 4</span>');
+		// 	$('.form-' + x).show();
+		// });
+	});
+});
+
+	$(".form").bind('submit', function (e){
+		e.preventDefault();
+
+		var dados = $(".form").serializeArray();
+
+
+		console.log(dados);
+
+		$('.form-5').html('<h3>Confira o resultado</h3><p> Primeira atividade - Você respondeu: <strong>' + dados[0].value + ' </strong> A resposta correta era Brazilian.</p>');
+
+
+
+	});
+
+var i = 0;
+$('.btn-speeding-2').on('click', function () {
+	if (i == 0) {
+		document.getElementById('audioTitle').play();
+		i++;
+	} else{
+		document.getElementById('audioTitle').pause();
+		i--;
+	}
+
+});
